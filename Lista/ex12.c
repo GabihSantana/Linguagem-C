542971496402a845ef5df3a832da2f5312ba9c9c
Gabriela Santana <gabriela2santtanna@gmail.com>
2023-08-21 11:47:44 -0300

    Practicing C

--- Lista/ex12.c
+++ Lista/ex12.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(){
-    int a, b, hip;
+    int a, b;
+    float hip;
 
-    printf("Insira o valor de A: ");
+    printf("\nInsira o valor de A: ");
     scanf(" %d", &a);
-    printf("Insira o valor de B: ");
+    printf("\nInsira o valor de B: ");
     scanf(" %d", &b);
 
+    hip = sqrt((a * a) + (b * b));
+    printf("\nA hipotenusa e igual a %.2f", hip);
 
 
     printf("\n\n\n");
